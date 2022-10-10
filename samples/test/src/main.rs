59be98f8ee5d0b4c9dcb02c356be4829189ad681
Michael Ilyin <milyin@gmail.com>
2022-10-10 09:49:46 +0200

    merge

--- samples/test/src/main.rs
+++ samples/test/src/main.rs
@@ -1,7 +1,10 @@
 use async_std::io::ReadExt;
 use serde::{Deserialize, Serialize};
 use zenoh::prelude::{r#async::AsyncResolve, Config, KeyExpr};
-use zshare::{get_data_path, get_update_path, Update, ZSharedValue, ZSharedView};
+use zshare::{
+    get_data_path, get_update_path, query_instances, query_instances_async, Update, ZSharedValue,
+    ZSharedView, INSTANCE, get_instance_path,
+};
 
 #[derive(Default, Serialize, Deserialize)]
 struct Value(u64);
@@ -33,13 +36,16 @@ async fn main() -> zshare::Result<()> {
     let workspace = KeyExpr::new("workspace")?;
     let name = KeyExpr::new("name")?;
     let data = ZSharedValue::new(&session, Value(42), &workspace, name.clone())?;
-    let view = ZSharedView::<Value, Change>::new(&session, &workspace, name.clone())?;
+    let view =
+        ZSharedView::<Value, Change>::new(&session, &workspace, INSTANCE.clone(), name.clone())?;
 
     println!(
-        "Commands: p, i, d, q\n{}\n{}",
-        get_data_path(&workspace, &name)?,
-        get_update_path(&workspace, &name)?
+        "Commands: p, i, d, q\n{}\n{}\n{}",
+        get_instance_path(&workspace, &INSTANCE, &name)?,
+        get_data_path(&workspace, &INSTANCE, &name)?,
+        get_update_path(&workspace, &INSTANCE, &name)?
     );
+
     let mut stdin = async_std::io::stdin();
     let mut input = [0_u8];
     loop {
@@ -49,6 +55,15 @@ async fn main() -> zshare::Result<()> {
             b'i' => data.update(Change::Inc),
             b'd' => data.update(Change::Dec),
             b'p' => println!("{} {}", data.read().0, view.read().0),
+            b's' => {
+                let instances = query_instances_async(&session, &workspace, &name).await?;
+                if !instances.is_empty() {
+                    println!("Instances:");
+                    for v in &instances {
+                        println!("{}", v);
+                    }
+                }
+            },
             _ => ()
             // 0 => sleep(Duration::from_secs(1)).await,
             // _ => subscriber.pull().res().await.unwrap(),
